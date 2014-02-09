627bebe94a7de1aa5e6afa944cbfd51b8bc67401
Chris Spooner <cmspooner@gmail.com>
2014-02-09 12:01:04 -0500

    Improved debuging info

--- Arduino/Serial_Drive/Serial_Drive.ino
+++ Arduino/Serial_Drive/Serial_Drive.ino
@@ -12,17 +12,20 @@ AF_DCMotor motor240(1);
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
-  Serial.println("Motor test!");
+  Serial.println("Waiting for Serial Data");
 }
 
 void loop() {
-  rawMotorDrive(0, 255, -255);
+  rawMotorDrive(2,0,0, false);
+  rawMotorDrive(255,0,-255);
+  rawMotorDrive(0,-120, 190);
+  rawMotorDrive(-270,500,0);
+  delay(1000);
 }
 
 void rawMotorDrive(int m0, int m120, int m240){
  if (m0 > 0){
   motor0.run(FORWARD);
-  Serial.println("Motor 0 Forward");
   if (m0 < 255){
     motor0.setSpeed(m0);
   } else {
@@ -30,7 +33,6 @@ void rawMotorDrive(int m0, int m120, int m240){
   }
  } else if (m0 < 0){
    motor0.run(BACKWARD);
-   Serial.println("Motor 0 Backward");
   if (m0 > -255){
     motor0.setSpeed(m0);
   } else {
@@ -38,12 +40,10 @@ void rawMotorDrive(int m0, int m120, int m240){
   }
  } else {
    motor0.run(RELEASE);
-   Serial.println("Motor 0 Stop");
  }
  
  if (m120 > 0){
   motor120.run(FORWARD);
-  Serial.println("Motor 120 Forward");
   if (m120 < 255){
     motor120.setSpeed(m120);
   } else {
@@ -51,7 +51,6 @@ void rawMotorDrive(int m0, int m120, int m240){
   }
  } else if (m120 < 0){
    motor120.run(BACKWARD);
-   Serial.println("Motor 120 Backward");
   if (m120 > -255){
     motor120.setSpeed(m120);
   } else {
@@ -59,12 +58,10 @@ void rawMotorDrive(int m0, int m120, int m240){
   }
  } else {
    motor120.run(RELEASE);
-   Serial.println("Motor 120 Stop");
  }
  
  if (m240 > 0){
   motor240.run(FORWARD);
-  Serial.println("Motor 240 Forward");
   if (m240 < 255){
     motor240.setSpeed(m240);
   } else {
@@ -72,7 +69,6 @@ void rawMotorDrive(int m0, int m120, int m240){
   }
  } else if (m240 < 0){
    motor240.run(BACKWARD);
-   Serial.println("Motor 240 Backward");
   if (m240 > -255){
     motor240.setSpeed(m240);
   } else {
@@ -80,7 +76,86 @@ void rawMotorDrive(int m0, int m120, int m240){
   }
  } else {
    motor240.run(RELEASE);
-   Serial.println("Motor 240 Stop");
+ }
+ 
+}
+
+void rawMotorDrive(int m0, int m120, int m240, boolean debug){
+ if (m0 > 0){
+  motor0.run(FORWARD);
+  if (debug) Serial.print("M0 F: ");
+  if (m0 < 255){
+    motor0.setSpeed(m0);
+    if (debug) Serial.print(m0);
+  } else {
+    motor0.setSpeed(255);
+    if (debug) Serial.print(255);
+  }
+ } else if (m0 < 0){
+   motor0.run(BACKWARD);
+   if (debug) Serial.print("M0 B: ");
+  if (m0 > -255){
+    motor0.setSpeed(m0);
+    if (debug) Serial.print(m0);
+  } else {
+    motor0.setSpeed(255);
+    if (debug) Serial.print(255);
+  }
+ } else {
+   motor0.run(RELEASE);
+   if (debug) Serial.print("M0 Stop");
+ }
+ if (debug) Serial.print(", ");
+ 
+ if (m120 > 0){
+  motor120.run(FORWARD);
+  if (debug) Serial.print("M120 F: ");
+  if (m120 < 255){
+    motor120.setSpeed(m120);
+    if (debug) Serial.print(m120);
+  } else {
+    motor120.setSpeed(255);
+    if (debug) Serial.print(255);
+  }
+ } else if (m120 < 0){
+   motor120.run(BACKWARD);
+   if (debug) Serial.print("M120 B: ");
+  if (m120 > -255){
+    motor120.setSpeed(m120);
+    if (debug) Serial.print(m120);
+  } else {
+    motor120.setSpeed(255);
+    if (debug) Serial.print(255);
+  }
+ } else {
+   motor120.run(RELEASE);
+   if (debug) Serial.print("M120 Stop");
+ }
+ if (debug) Serial.print(", ");
+ 
+ if (m240 > 0){
+  motor240.run(FORWARD);
+  if (debug) Serial.print("M240 F: ");
+  if (m240 < 255){
+    motor240.setSpeed(m240);
+    if (debug) Serial.println(m240);
+  } else {
+    motor240.setSpeed(255);
+    if (debug) Serial.println(255);
+  }
+ } else if (m240 < 0){
+   motor240.run(BACKWARD);
+   if (debug) Serial.print("M240 B: ");
+  if (m240 > -255){
+    motor240.setSpeed(m240);
+    if (debug) Serial.println(m240);
+  } else {
+    motor240.setSpeed(255);
+    if (debug) Serial.println(m240);
+  }
+ } else {
+   motor240.run(RELEASE);
+   if (debug) Serial.println("M240 Stop");
  }
  
 }
