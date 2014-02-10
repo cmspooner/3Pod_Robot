44c7ad572899488593b102a508688a8e3bf753f3
Chris Spooner <cspooner@kearsarge.org>
2014-02-10 13:37:44 -0500

    Start of Python ThreePod object

---
+++ Python/ThreePod.py
@@ -0,0 +1,38 @@
+class ThreePod():
+	def __init__(self):
+		#Cartesian Coordinate System 
+		self.speedx = 0
+		self.speedy = 0
+		
+		#Polar Coordinate System
+		self.angle = 0
+		self.speed  = 0
+		
+		#Rotation on Axis
+		self.rotation = 0
+		
+	def __str__(self, coordSys = "polar"):
+		if coodSys == "polar":
+			return self.speed, self.angle
+		if coodSys == "xy":
+			return self.speedx, self.speedy, self.rot
+			
+	def movePolar(speed, angle)
+		self.speed = speed
+		self.angle = angle
+		
+		#self.speedx = ~Math
+		#self.speedy = ~Math
+		
+	def moveXY(x, y):
+		self.speedx = x
+		self.speedy = y
+		
+		#self.speed = ~Math
+		#self.angle = ~Math
+		
+	def update():
+		moveString = ""
+		
+		return moveString
+		
